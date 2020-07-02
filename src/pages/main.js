c35d654cb25be1476f65bfdaf6c0390fe6279215
ArthurSantos20 <artiesn_2011@hotmail.com>
2020-07-01 23:03:47 -0300

    Made DB.json

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import Table from '../components/table'
 
+const API_URL = "https://my-json-server.typicode.com/ArthurSantos20/petzTeste";
 
 export default class Main extends React.Component {
+
+	fetch(`${API_URL}/cursos`)
+  	.then(response => response.json())
+ 	.then(cursos => listaCursos(cursos))
+
+	function listaCursos(cursos) {
+	  cursos.forEach(({titulo, url}) => {
+	      const a = criaNovaAncoraComURL(titulo, url);
+	      const li = criaNovoItemComAncora(a);
+
+	      $cursos.append(li);
+	  })
+	}
+
 	render() {
 		return (
 			<div>
